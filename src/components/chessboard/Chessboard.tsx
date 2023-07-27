8971cd5b9ce9a684ea5376f23aa2c86615e3ea11
Akshat-shah05 <akshatshah1605@gmail.com>
2023-07-27 01:18:55 -0400

    vertical label completed

--- src/components/chessboard/Chessboard.tsx
+++ src/components/chessboard/Chessboard.tsx
@@ -25,15 +25,22 @@ const Chessboard = () => {
     hLabel.push(<div className="tile-label green-tile">{horizontalAxis[i]}</div>)
   }
 
+  for (let i = verticalAxis.length-1; i >= 0; i--) {
+    vLabel.push(<div className="tile-label2 white-tile">{verticalAxis[i]}</div>)
+  }
+
 
 
   return (
     <>
-        <div id="chessboard-container">
-            <div id="chessboard">{board}</div>
+      <div id="chessboard-container">
+        <div className="vertical-label">{vLabel}</div>
+        <div className="c_n_l_container">
+          <div id="chessboard">{board}</div>
+          <div className="horizontal-label">{hLabel}</div>  
         </div>
-        <div className="horizontal-label">{hLabel}</div>  
-        
+      </div>
+       
     </>
   );
 };

73c321c97711217c0f1d589cc095d4ea31887790
Akshat-shah05 <akshatshah1605@gmail.com>
2023-07-27 00:28:41 -0400

    comment

---
+++ src/components/chessboard/Chessboard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Chessboard.css";
+
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+
+const Chessboard = () => {
+  let board = [];
+
+  {/* Loop to produce the chessboard */}
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const number = j + i + 2;
+      number % 2 === 0
+        ? board.push(<div className="tile green-tile"></div>)
+        : board.push(<div className="tile white-tile"></div>);
+    }
+  }
+
+  let hLabel = [];
+  let vLabel = [];
+
+  for (let i = 0; i<horizontalAxis.length; i++) {
+    hLabel.push(<div className="tile-label green-tile">{horizontalAxis[i]}</div>)
+  }
+
+
+
+  return (
+    <>
+        <div id="chessboard-container">
+            <div id="chessboard">{board}</div>
+        </div>
+        <div className="horizontal-label">{hLabel}</div>  
+    </>
+  );
+};
+
+export default Chessboard;
